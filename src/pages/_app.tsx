b4431732c05a73ab917cd92a36b672737dd4764e
15997416 <akbar.zakki@muf.co.id>
2025-04-27 08:04:11 +0700

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/styles/global.css' // Pastikan path-nya sesuai
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Sura(Ja)karta</title>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
